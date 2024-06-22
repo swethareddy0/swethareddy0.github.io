1e283c9798eb9dedf10b19ddef77f32ae18e9c25
Swetha Reddy Vuppala <60864843+swethareddy0@users.noreply.github.com>
2024-06-21 21:42:34 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 
+// Custom JavaScript code for sticky navigation
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 50) {
+            $('header').addClass('navbar-scrolled');
+        } else {
+            $('header').removeClass('navbar-scrolled');
+        }
+    });
+});
